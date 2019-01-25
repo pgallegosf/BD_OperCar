d9773a25d906b9725771783979a2126aaf2bebf2
pgallegos <pool.gallegos@efitec.pe>
2019-01-25 18:39:18 -0500

    creacion de tablas

---
+++ OpeCar/OpeCar/auditoria/Tables/AccesoError.sql
@@ -0,0 +1,42 @@
+﻿CREATE TABLE [auditoria].[AccesoError] (
+    [Id]            CHAR (26)     NOT NULL,
+    [Usuario]       VARCHAR (20)  NOT NULL,
+    [HostAdress]    VARCHAR (20)  NULL,
+    [HostName]      VARCHAR (20)  NULL,
+    [Agente]        VARCHAR (100) NULL,
+    [Codigo]        VARCHAR (10)  NULL,
+    [Descripcion]   TEXT          NOT NULL,
+    [Pila]          TEXT          NULL,
+    [Excepcion]     TEXT          NULL,
+    [FechaCreacion] DATETIME      NOT NULL,
+    CONSTRAINT [PK_AccesoError] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Fecha que se crea el registro.', @level0type = N'SCHEMA', @level0name = N'auditoria', @level1type = N'TABLE', @level1name = N'AccesoError', @level2type = N'COLUMN', @level2name = N'FechaCreacion';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Serializado de la excepción.', @level0type = N'SCHEMA', @level0name = N'auditoria', @level1type = N'TABLE', @level1name = N'AccesoError', @level2type = N'COLUMN', @level2name = N'Excepcion';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Rastro de llamadas en caso de error.', @level0type = N'SCHEMA', @level0name = N'auditoria', @level1type = N'TABLE', @level1name = N'AccesoError', @level2type = N'COLUMN', @level2name = N'Pila';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Descripción.', @level0type = N'SCHEMA', @level0name = N'auditoria', @level1type = N'TABLE', @level1name = N'AccesoError', @level2type = N'COLUMN', @level2name = N'Descripcion';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Código.', @level0type = N'SCHEMA', @level0name = N'auditoria', @level1type = N'TABLE', @level1name = N'AccesoError', @level2type = N'COLUMN', @level2name = N'Codigo';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Nombre de usuario', @level0type = N'SCHEMA', @level0name = N'auditoria', @level1type = N'TABLE', @level1name = N'AccesoError', @level2type = N'COLUMN', @level2name = N'Usuario';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Código de log', @level0type = N'SCHEMA', @level0name = N'auditoria', @level1type = N'TABLE', @level1name = N'AccesoError', @level2type = N'COLUMN', @level2name = N'Id';
+
---
+++ OpeCar/OpeCar/seguridad/Tables/Permiso.sql
@@ -0,0 +1,51 @@
+﻿CREATE TABLE [seguridad].[Permiso] (
+    [IdUsuario]             INT      NOT NULL,
+    [IdRol]                 TINYINT  NOT NULL,
+    [IdHistorico]           INT      NOT NULL,
+    [FechaIniVig]           DATETIME NOT NULL,
+    [FechaFinVig]           DATETIME NULL,
+    [IdUsuarioCreacion]     INT      NOT NULL,
+    [IdUsuarioModificacion] INT      NULL,
+    [FechaCreacion]         DATETIME NOT NULL,
+    [FechaModificacion]     DATETIME NULL,
+    CONSTRAINT [PK_Permiso] PRIMARY KEY CLUSTERED ([IdUsuario] ASC, [IdRol] ASC, [IdHistorico] ASC),
+    CONSTRAINT [FK_Permiso_Rol] FOREIGN KEY ([IdRol]) REFERENCES [seguridad].[Rol] ([IdRol]),
+    CONSTRAINT [FK_Permiso_Usuario] FOREIGN KEY ([IdUsuario]) REFERENCES [seguridad].[Usuario] ([IdUsuario])
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Fecha que se realiza la ultima modificación.', @level0type = N'SCHEMA', @level0name = N'seguridad', @level1type = N'TABLE', @level1name = N'Permiso', @level2type = N'COLUMN', @level2name = N'FechaModificacion';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Fecha en crea el registro.', @level0type = N'SCHEMA', @level0name = N'seguridad', @level1type = N'TABLE', @level1name = N'Permiso', @level2type = N'COLUMN', @level2name = N'FechaCreacion';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Código de usuario qui hizo la ultima modificación al registro.', @level0type = N'SCHEMA', @level0name = N'seguridad', @level1type = N'TABLE', @level1name = N'Permiso', @level2type = N'COLUMN', @level2name = N'IdUsuarioModificacion';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Identificador del usuario que crea el registro.', @level0type = N'SCHEMA', @level0name = N'seguridad', @level1type = N'TABLE', @level1name = N'Permiso', @level2type = N'COLUMN', @level2name = N'IdUsuarioCreacion';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Fecha fin de vigencia del registro.', @level0type = N'SCHEMA', @level0name = N'seguridad', @level1type = N'TABLE', @level1name = N'Permiso', @level2type = N'COLUMN', @level2name = N'FechaFinVig';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Fecha inicio de vigencia del registro.', @level0type = N'SCHEMA', @level0name = N'seguridad', @level1type = N'TABLE', @level1name = N'Permiso', @level2type = N'COLUMN', @level2name = N'FechaIniVig';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Código de historico.', @level0type = N'SCHEMA', @level0name = N'seguridad', @level1type = N'TABLE', @level1name = N'Permiso', @level2type = N'COLUMN', @level2name = N'IdHistorico';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Código del rol', @level0type = N'SCHEMA', @level0name = N'seguridad', @level1type = N'TABLE', @level1name = N'Permiso', @level2type = N'COLUMN', @level2name = N'IdRol';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Código de usuario', @level0type = N'SCHEMA', @level0name = N'seguridad', @level1type = N'TABLE', @level1name = N'Permiso', @level2type = N'COLUMN', @level2name = N'IdUsuario';
+
